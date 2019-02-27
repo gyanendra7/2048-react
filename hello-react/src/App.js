e9015438384cfaabfe730662a80b242021c8cb78
gyanendra7 <gyanendra@gradeup.co>
2019-02-27 18:36:45 +0530

    react 7.0

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,99 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Jumbotron, Button, Container, Row, Col, Card } from 'reactstrap';
 
-function getColor(value) {
-  let color = 'grey';
-  switch(value) {
-    case 2:
-      color = 'grey';
-      break;
-    case 4:
-      color = 'white';
-      break;
-    case 8:
-      color = 'pink';
-      break;
-    case 16:
-      color = 'yellow';
-      break;  
-    case 32:
-      color = 'red';
-      break;
-    case 64:
-      color = 'green';
-      break;
-    case 128:
-      color = 'cyan';
-      break;
-    case 256:
-      color = 'blue';
-      break; 
-    case 512:
-      color = 'magenta';
-      break;  
-    case 1024:
-      color = 'purple';
-      break;
-    case 2048:
-      color = 'Crimson';
-      break;
-    case 4096:
-      color = 'LightSalmon';
-      break;
-    case 8192:
-      color = 'grey';
-      break;  
-    default:
-      color = 'FireBrick';
-  }
-  return color;
-}
 
 function Square(props) {
   return (
-    <button className="square" style={{ backgroundColor: `${getColor(props.value)}`}} onClick={props.onClick}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
 
-// function Square(props) {
-//   return (
-//     <button className="square" style={{ backgroundColor: `${props.value == 2 ? 'red' : 'green'}`}} onClick={props.onClick}>
-//       {props.value}
-//     </button>
-//   );
-// }
-
 class Board extends Component {
   renderSquare(i, j) {
-    let value = this.props.squares[i][j];
-    if (value === 0) value = null;
     return (
-      <Square value={value} />
+      <Square
+        value={this.props.squares[i][j]}
+      />
     );
   }
 
   render() {
     return (
-      <div className="board-row">
-        {this.renderSquare(0, 0)}
-        {this.renderSquare(0, 1)}
-        {this.renderSquare(0, 2)}
-        {this.renderSquare(0, 3)}
-        {this.renderSquare(1, 0)}
-        {this.renderSquare(1, 1)}
-        {this.renderSquare(1, 2)}
-        {this.renderSquare(1, 3)}
-        {this.renderSquare(2, 0)}
-        {this.renderSquare(2, 1)}
-        {this.renderSquare(2, 2)}
-        {this.renderSquare(2, 3)}
-        {this.renderSquare(3, 0)}
-        {this.renderSquare(3, 1)}
-        {this.renderSquare(3, 2)}
-        {this.renderSquare(3, 3)}
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0, 0)}
+          {this.renderSquare(0, 1)}
+          {this.renderSquare(0, 2)}
+          {this.renderSquare(0, 3)}
+          {this.renderSquare(1, 0)}
+          {this.renderSquare(1, 1)}
+          {this.renderSquare(1, 2)}
+          {this.renderSquare(1, 3)}
+          {this.renderSquare(2, 0)}
+          {this.renderSquare(2, 1)}
+          {this.renderSquare(2, 2)}
+          {this.renderSquare(2, 3)}
+          {this.renderSquare(3, 0)}
+          {this.renderSquare(3, 1)}
+          {this.renderSquare(3, 2)}
+          {this.renderSquare(3, 3)}
+        </div>
       </div>
     );
   }
@@ -106,50 +52,16 @@ class Game extends React.Component {
       squares: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, 2]],
       isGameOver: false,
       changeOccured: false,
-      score: 0,
-      time: 0,
-      start: Date.now(),
-      bestScore: 0,
     };
     this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleRestart = this.handleRestart.bind(this);
     this.shiftOneD = this.shiftOneD.bind(this);
     this.shiftLeft = this.shiftLeft.bind(this);
-    this.startTimer = this.startTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
-    this.getTime = this.getTime.bind(this);
-  }
-
-  startTimer() {
-    this.setState({
-      time: this.state.time,
-      start: Date.now()
-    })
-    this.timer = setInterval(() => this.setState({
-      time: Date.now() - this.state.start
-    }), 1)
-    console.log("start")
-  }
-
-  resetTimer() {
-    this.setState({ time: 0, start: Date.now() })
-  }
-
-  handleRestart() {
-    this.resetTimer()
-    this.setState(() => ({
-      squares: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, 2]],
-      isGameOver: false,
-      changeOccured: false,
-      score: 0,
-    }));
   }
 
   shiftOneD(arr) {
     let lastNonSetIndex = 3;
     let index = 2;
     let flag = false;
-    let score = this.state.score;
     while(index >= 0) {
       if (arr[lastNonSetIndex] === 0) {
         arr[lastNonSetIndex] = arr[index];
@@ -161,7 +73,6 @@ class Game extends React.Component {
       }
       else if (arr[lastNonSetIndex] === arr[index]) {
         arr[lastNonSetIndex] = 2 * arr[index];
-        score += 2 * arr[index];
         arr[index] = 0;
         flag = true;
         lastNonSetIndex--;
@@ -178,10 +89,6 @@ class Game extends React.Component {
       }
     }
     this.state.changeOccured = flag;
-    this.state.score = score;
-    if (score > this.state.bestScore) {
-      this.state.bestScore = score;
-    }
     console.log({ arr });
     return arr;
   }
@@ -189,76 +96,71 @@ class Game extends React.Component {
   shiftRight(squares) {
     console.log('shiftLeft');
     console.log({ squares });
-    let changeOccuredFlag = false;
-    this.state.changeOccured = false;
+    let changeOccuredFlag = true;
     for (var i = 0; i < squares.length ; i++) {
       let oneDArr = [squares[i][0], squares[i][1], squares[i][2], squares[i][3]];
       oneDArr = this.shiftOneD(oneDArr);
-      changeOccuredFlag = this.state.changeOccured || changeOccuredFlag;
+      changeOccuredFlag = this.state.changeOccured && changeOccuredFlag;
       console.log({ oneDArr });
       squares[i][0] = oneDArr[0];
       squares[i][1] = oneDArr[1];
       squares[i][2] = oneDArr[2];
-      squares[i][3] = oneDArr[3];
+      squares[i][3] = oneDArr[3]; 
     }
-    this.state.changeOccured = changeOccuredFlag;
+    this.state.changeOccured = changeOccuredFlag; 
     return squares;
   }
 
   shiftLeft(squares) {
     console.log({ squares });
-    let changeOccuredFlag = false;
-    this.state.changeOccured = false;
+    let changeOccuredFlag = true;
     for (let i = 0; i < squares.length ; i++) {
       let oneDArr = [squares[i][3], squares[i][2], squares[i][1], squares[i][0]];
       oneDArr = this.shiftOneD(oneDArr);
-      changeOccuredFlag = this.state.changeOccured || changeOccuredFlag;
+      changeOccuredFlag = this.state.changeOccured && changeOccuredFlag;
       squares[i][0] = oneDArr[3];
       squares[i][1] = oneDArr[2];
       squares[i][2] = oneDArr[1];
-      squares[i][3] = oneDArr[0];
+      squares[i][3] = oneDArr[0]; 
     }
-    this.state.changeOccured = changeOccuredFlag;
+    this.state.changeOccured = changeOccuredFlag; 
     return squares;
   }
 
   shiftDown(squares) {
     console.log('shiftUp');
-    let changeOccuredFlag = false;
-    this.state.changeOccured = false;
+    let changeOccuredFlag = true;
     for (let i = 0; i < squares.length ; i++) {
       let oneDArr = [squares[0][i], squares[1][i], squares[2][i], squares[3][i]];
       oneDArr = this.shiftOneD(oneDArr);
-      changeOccuredFlag = this.state.changeOccured || changeOccuredFlag;
+      changeOccuredFlag = this.state.changeOccured && changeOccuredFlag;
       squares[0][i] = oneDArr[0];
       squares[1][i] = oneDArr[1];
       squares[2][i] = oneDArr[2];
-      squares[3][i] = oneDArr[3];
+      squares[3][i] = oneDArr[3]; 
     }
-    this.state.changeOccured = changeOccuredFlag;
+    this.state.changeOccured = changeOccuredFlag; 
     return squares;
   }
 
   shiftUp(squares) {
-    let changeOccuredFlag = false;
-    this.state.changeOccured = false;
+    let changeOccuredFlag = true;
     for (let i = 0; i < squares.length ; i++) {
       let oneDArr = [squares[3][i], squares[2][i], squares[1][i], squares[0][i]];
       oneDArr = this.shiftOneD(oneDArr);
-      changeOccuredFlag = this.state.changeOccured ||changeOccuredFlag;
+      changeOccuredFlag = this.state.changeOccured && changeOccuredFlag;
       squares[0][i] = oneDArr[3];
       squares[1][i] = oneDArr[2];
       squares[2][i] = oneDArr[1];
-      squares[3][i] = oneDArr[0];
+      squares[3][i] = oneDArr[0]; 
     }
-    this.state.changeOccured = changeOccuredFlag;
+    this.state.changeOccured = changeOccuredFlag; 
     return squares;
   }
 
   handleKeyDown(key) {
     /* code for shifting array */
     let shiftArr = this.state.squares;
-    if (this.state.isGameOver) return;
     console.log({ key: key.keyCode });
     switch (key.keyCode) {
       case 37:
@@ -305,108 +207,41 @@ class Game extends React.Component {
     });
   }
 
-  getTime() {
-    const timeInSeconds = Math.floor(this.state.time / 1000);
-    const minutes = Math.floor(timeInSeconds / 60) || 0;
-    const seconds = timeInSeconds % 60 || 0;
-    console.log({ time: `${minutes}:${seconds}` });
-    return `${minutes}:${seconds}`;
-  } 
-
   render() {
-    const { squares, score }= this.state;
+    // const history = this.state.history;
+    // const current = history[this.state.stepNumber];
+    const squares = this.state.squares;
     const isGameOver = checkGameOver(squares);
-    console.log({ squares, score });
+
     const status = isGameOver ? 'Game Over' : 'Play!';
     console.log({ status });
     return (
-      <Container>
-      <div id="header" name="header">
-      <Card id="cardname" name="cardname">Gradeup 2048 Game!</Card>
-      </div>
-      <div>
-      <div className="game" onKeyDown={this.handleKeyDown} tabIndex="0">
-      <Row>
-        <Col>
-          <Row>
-            <Col></Col>
-            <Col>
-              <div className="game-board">
-                <Board squares={squares}/>
-              </div>
-            </Col>
-            <Col></Col>
-          </Row>
-        <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-        <Col></Col>
-              <Col>
-                <div className="game-info">
-                  <Button color="danger" onClick={this.startTimer}>{status}</Button>
-                </div>
-              </Col>
-              <Col></Col>
-              <Col>
-                <div className="score">
-                  <Button color="warning">Score: {score}!</Button>
-                </div>
-              </Col>
-              <Col></Col>
-              <Col>
-                <div className="restart">
-                  <Button color="info" onClick={this.handleRestart}>New Game!</Button>
-                </div>
-              </Col>
-              <Col></Col><Col></Col><Col></Col><Col></Col><Col></Col><Col></Col><Col></Col>
-            </Row>
-          </Col>
-        </Row>
-        <div id="timer" name="timer">
-        <Row>
-        <Col></Col><Col></Col><Col></Col>
-        <Col><div className="timer">
-          <Button color="success"> {this.getTime()}</Button>
-        </div></Col><Col></Col><Col></Col><Col></Col>
-        </Row>
+      <div 
+        className="game"
+        onKeyDown={this.handleKeyDown}
+        tabIndex="0" 
+      >
+        <div className="game-board">
+          <Board
+            squares={squares}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
         </div>
       </div>
-      </div>
-      </Container>
     );
   }
 }
 
-function checkSafeMove(i, j) {
-  if (i < 0 || i > 3 || j < 0 || j > 3) return false;
-  return true;
-}
-
 function checkGameOver(squares) {
   let ifZeroPresent = false;
-  let ifPossibleMatch = false;
   for (let i = 0; i < squares.length; i++) {
     for(let j = 0; j < squares.length; j++) {
       if(squares[i][j] === 0) ifZeroPresent = true;
-      if (checkSafeMove(i + 1, j)) {
-        if (squares[i][j] === squares[i + 1][j]) ifPossibleMatch = true;
-      }
-      if (checkSafeMove(i, j + 1)) {
-        if (squares[i][j] === squares[i][j + 1]) ifPossibleMatch = true;
-      }
-      if (checkSafeMove(i, j - 1)) {
-        if (squares[i][j] === squares[i][j - 1]) ifPossibleMatch = true;
-      }
-      if (checkSafeMove(i - 1, j)) {
-        if (squares[i][j] === squares[i - 1][j]) ifPossibleMatch = true;
-      }
     }
   }
-  return (ifZeroPresent || ifPossibleMatch) ? false : true;
+  return ifZeroPresent ? false : true;
 }
 
 
